d534ddd7d5698049ffb13f2c76989294d622fb82
narrakas <zixing.narrakas1996@gmail.com>
2019-05-09 11:36:48 -0400

    Bug fix where Static Response, server details are not rendered in the HTML Response

--- src/resources/DynamicalWeb/Page.php
+++ src/resources/DynamicalWeb/Page.php
@@ -83,6 +83,8 @@
          */
         public static function staticResponse(string $title, string $header, string $body)
         {
+            $ServerInformation = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'dynamicalweb.json');
+            $ServerInformation = json_decode($ServerInformation, true);
             ?>
             <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
             <html lang="en">
@@ -93,7 +95,7 @@
                     <h1><?PHP HTML::print($header); ?></h1>
                     <p><?PHP HTML::print($body); ?></p>
                     <hr>
-                    <address>DynamicalWeb/1.0.0.0 (Intellivoid) Written by Zi Xing Narrakas</address>
+                    <address>DynamicalWeb/<?PHP HTML::print($ServerInformation['VERSION']); ?> (<?PHP HTML::print($ServerInformation['COMPANY']); ?>) Written by <?PHP HTML::print($ServerInformation['AUTHOR']); ?></address>
                 </body>
             </html>
             <?PHP
