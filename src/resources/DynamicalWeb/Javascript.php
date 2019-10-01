46efc61cb98c8e704a6a7c8df754bd3192e4beab
Netkas <45276434+Netkas@users.noreply.github.com>
2019-10-01 11:45:37 -0300

    Added JSMin

--- src/resources/DynamicalWeb/Javascript.php
+++ src/resources/DynamicalWeb/Javascript.php
@@ -4,7 +4,23 @@
     namespace DynamicalWeb;
 
 
+    use Exception;
+
+    /**
+     * Class Javascript
+     * @package DynamicalWeb
+     */
     class Javascript
     {
-
+        /**
+         * Compresses the Javascript Souce code and returns the compressed output
+         *
+         * @param string $src
+         * @return string
+         * @throws Exception
+         */
+        public static function minify(string $src): string
+        {
+            return JSMin::minify($src);
+        }
     }
